42b79c6d51128654a872100eb267c9356f72611e
mrimal <mrimal@worldbank.org>
2017-03-21 18:13:30 -0400

    One more edit - this time with 473 crops

--- scraping.py
+++ scraping.py
@@ -51,43 +51,46 @@ fromdate = '2012-01-01'
 todate = '2016-12-30'
 #commodityName = 'Yogurt'
 #Sending post requests to the website for the data
+veggies = pandas.read_csv('veggies2.csv')
 
-r = requests.post(config.url, data={'fromdate': fromdate, 'todate': todate, 'stype': 'commodity_wise', 'page':'commodity', 'commodity_english[]':commodityName})
-print(r.status_code, r.reason)
-soup = BeautifulSoup(r.text)
-print soup
+for vegs in veggies: 
+    
+    r = requests.post(config.url, data={'fromdate': fromdate, 'todate': todate, 'stype': 'commodity_wise', 'page':'commodity', 'commodity_english[]':vegs})
+    print(r.status_code, r.reason)
+    soup = BeautifulSoup(r.text)
+    print soup
 #print(r.text)
 #pages_list = soup.find_all('input', name ='pg')
 
-pages_list = soup.find_all(name='input', attrs={'name': 'pg', 'type': 'hidden'})
+    pages_list = soup.find_all(name='input', attrs={'name': 'pg', 'type': 'hidden'})
 #print(pages_list)
 
-a = 1 
+    a = 1 
 
-for pages in pages_list :
-    a = a + 1
-
-print a 
-
-try:
-    table = soup.find('table', {"class": "table"})
-    rows = table.find_all('tr')
-except AttributeError as e:
-    print 'No table found'
-
-#print(table)
-results = []
-
-for row in rows :
-    table_headers = row.find_all('th')
-    if table_headers:
-        results.append([headers.get_text() for headers in table_headers])
+    for pages in pages_list :
+        a = a + 1
     
-    table_data = row.find_all('td', attrs={})    
-    if table_data:
-        results.append([data.get_text() for data in table_data])
-#print(results)        
-final_table = pandas.DataFrame(results, index=None)
+    print a 
+    
+    try:
+        table = soup.find('table', {"class": "table"})
+        rows = table.find_all('tr')
+    except AttributeError as e:
+        print 'No table found'
+    
+    #print(table)
+    results = []
+    
+    for row in rows :
+        table_headers = row.find_all('th')
+        if table_headers:
+            results.append([headers.get_text() for headers in table_headers])
+        
+        table_data = row.find_all('td', attrs={})    
+        if table_data:
+            results.append([data.get_text() for data in table_data])
+    #print(results)        
+    final_table = pandas.DataFrame(results, index=None)
 
 #writer = pandas.ExcelWriter('output.xlsx')
 #final_table.to_excel(writer,'Sheet1')
@@ -96,16 +99,16 @@ final_table = pandas.DataFrame(results, index=None)
 
 #Trying to loop over all the available page numbers and then appending the data to the excel file. 
 
-for i in range(2,a):
-    r = requests.post(config.url, data={'fromdate': fromdate, 'todate': todate, 'stype': 'commodity_wise', 'page':'commodity', 'commodity_english[]':commodityName, 'pg': i})
-    print(r.status_code, r.reason)
-    soup = BeautifulSoup(r.text)
-    print i
-    try:
-        table = soup.find('table', {"class": "table"})
-        rows = table.find_all('tr')
-    except AttributeError as e:
-        print 'No table found'
+    for i in range(2,a):
+        r = requests.post(config.url, data={'fromdate': fromdate, 'todate': todate, 'stype': 'commodity_wise', 'page':'commodity', 'commodity_english[]':vegs, 'pg': i})
+        print(r.status_code, r.reason)
+        soup = BeautifulSoup(r.text)
+        print i
+        try:
+            table = soup.find('table', {"class": "table"})
+            rows = table.find_all('tr')
+        except AttributeError as e:
+            print 'No table found'
 
 #print(table)
    # resultsNew = []
