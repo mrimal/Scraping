fbf0832530ad884765cb6dd99416b1f8846cddd9
mrimal <mrimal@worldbank.org>
2017-03-21 11:33:46 -0400

    Looping over the pages
    More plugging in

--- scraping.py
+++ scraping.py
@@ -48,9 +48,10 @@ z = br.open(config.url).read()
 #print(z)
 fromdate = '2012-01-01'
 todate = '2016-12-30'
+commodityName = 'Yogurt'
 #Sending post requests to the website for the data
 
-r = requests.post(config.url, data={'fromdate': fromdate, 'todate': todate, 'stype': 'commodity_wise', 'page':'commodity', 'commodity_english[]':'Yogurt'})
+r = requests.post(config.url, data={'fromdate': fromdate, 'todate': todate, 'stype': 'commodity_wise', 'page':'commodity', 'commodity_english[]':commodityName})
 print(r.status_code, r.reason)
 soup = BeautifulSoup(r.text)
 print soup
@@ -58,9 +59,9 @@ print soup
 #pages_list = soup.find_all('input', name ='pg')
 
 pages_list = soup.find_all(name='input', attrs={'name': 'pg', 'type': 'hidden'})
-print(pages_list)
+#print(pages_list)
 
-a = 0 
+a = 1 
 
 for pages in pages_list :
     a = a + 1
@@ -87,8 +88,40 @@ for row in rows :
 #print(results)        
 final_table = pandas.DataFrame(results, index=None)
 
-final_table.to_csv('newfile.csv',sep=",", index=False, columns=None, header=False)
+#final_table.to_csv('newfile.csv',sep=",", index=False, columns=None, header=False)
 
-#writer = pandas.ExcelWriter('output.xlsx')
-#final_table.to_excel(writer,'Sheet1')
-#writer.save()
+writer = pandas.ExcelWriter('output.xlsx')
+final_table.to_excel(writer,'Sheet1')
+writer.save()
+
+for i in range(2,a):
+    r = requests.post(config.url, data={'fromdate': fromdate, 'todate': todate, 'stype': 'commodity_wise', 'page':'commodity', 'commodity_english[]':commodityName, 'pg': i})
+    print(r.status_code, r.reason)
+    soup = BeautifulSoup(r.text)
+    print soup
+    try:
+        table = soup.find('table', {"class": "table"})
+        rows = table.find_all('tr')
+    except AttributeError as e:
+        print 'No table found'
+
+#print(table)
+    results = []
+
+    for row in rows :
+        table_headers = row.find_all('th')
+        if table_headers:
+            results.append([headers.get_text() for headers in table_headers])
+        
+        table_data = row.find_all('td', attrs={})    
+        if table_data:
+            results.append([data.get_text() for data in table_data])
+#print(results)        
+    final_table = pandas.DataFrame(results, index=None)
+
+    writer = pandas.ExcelWriter('output.xlsx')
+    final_table.to_excel(writer,'Sheet1')
+    writer.save()
+#for i in range(2,a):
+ #   print i
+    
